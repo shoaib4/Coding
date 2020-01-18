51441caa410e43b8d953834bb50375ed62127e77
Shoaib siddiquie M A <shoaib.m@imimobile.com>
2020-01-18 23:54:53 +0530

    done creating Adj list

--- Practice/Graphes/creating_Adj_list.cpp
+++ Practice/Graphes/creating_Adj_list.cpp
@@ -1,12 +1,28 @@
 #include<iostream> 
 #include<bits/stdc++.h>
 using namespace std; 
-void creatAdjList(int n,vector<int> E1, vector<int> E2){
-    
-    map<int,set<int>> Adj;
+
+void printAdjLink(map< int,set<int> > Adj){
+    for(pair<int,set<int> > S : Adj){
+        for(auto x : S.second){
+            cout << x << " ";
+        }
+        cout << endl;
+    }
+}
+void creatAdjList(int n,vector<int> A, vector<int> B){
+    map<int,set<int> > Adj;
+    for(int i=0;i<A.size();i++){
+        Adj[A[i]].insert(B[i]);
+    } 
+    printAdjLink(Adj);
 }
 
 int main(){
+    if( __cplusplus == 201103L ) std::cout << "C++11\n" ;
+else if( __cplusplus == 19971L ) std::cout << "C++98\n" ;
+else std::cout << "pre-standard C++\n" ;
+
     int n;
     int e;
     cin >> n; cin >> e; 
